15cc8056d59e52ffaaa985c565059876e7d6b9a6
phette23 <phette23@gmail.com>
2014-03-01 14:09:38 -0500

    routes/index - normalize class number param in URL

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,16 @@ exports.index = function(req, res){
 };
 
 exports.number = function(req, res){
-    var classNumber = parseInt(req.params.number);
+    var classNumber = req.params.number;
+
+    // normalize improper class numbers
+    if (classNumber.length > 3) {
+        classNumber = classNumber.substr(0, 3);
+    } else if (classNumber.length === 2) {
+        classNumber = '0' + classNumber;
+    } else if (classNumber.length === 1) {
+        classNumber = '00' + classNumber;
+    }
 
     dogedc(classNumber, function(ddc) {
         res.render('index', {
