774522efa9660147796856ccece509c1e60c21d8
phette23 <phette23@gmail.com>
2014-03-01 15:41:23 -0500

    handle improper classes passed in URL, e.g. non-digit strings

--- routes/index.js
+++ routes/index.js
@@ -12,13 +12,10 @@ exports.index = function(req, res){
 exports.number = function(req, res){
     var classNumber = req.params.number;
 
-    // normalize improper class numbers
-    if (classNumber.length > 3) {
-        classNumber = classNumber.substr(0, 3);
-    } else if (classNumber.length === 2) {
-        classNumber = '0' + classNumber;
-    } else if (classNumber.length === 1) {
-        classNumber = '00' + classNumber;
+    // improper classNumber
+    if (!classNumber.match(/^\d{3}$/)) {
+        res.redirect('/');
+        return;
     }
 
     dogedc(classNumber, function(ddc) {
