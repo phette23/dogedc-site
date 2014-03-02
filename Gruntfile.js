bf9b21680532b9ca7f58abca279a4d9d71cf8fbd
phette23 <phette23@gmail.com>
2014-03-02 18:54:44 -0500

    working grunt tasks for cssmin & csslint

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  grunt.initConfig({
+    cssmin: {
+      minify: {
+        expand: true,
+        cwd: 'public/stylesheets/',
+        src: ['style.css'],
+        dest: 'public/stylesheets/',
+        ext: '.min.css'
+      }
+    },
+    csslint: {
+      strict: {
+        options: {
+          // used in micro-clearfix
+          "star-property-hack": false
+        },
+        src: ['public/stylesheets/style.css']
+      }
+    }
+  });
+
+  grunt.registerTask('test', ['csslint']);
+  grunt.registerTask('build', ['cssmin']);
+  grunt.registerTask('default', ['test', 'build']);
+
+};
