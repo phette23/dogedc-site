51aedae7163903423a05e1ad8a2bba57d71ce9e5
phette23 <phette23@gmail.com>
2017-07-12 21:25:19 -0700

    update server app to Express 4

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var express = require('express');
+var favicon = require('serve-favicon')
+var logger = require('morgan')
+var compression = require('compression')
+var errorHandler = require('errorhandler')
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
@@ -8,27 +12,25 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon(path.join(__dirname, 'public/images/favicon.ico')));
-app.use(express.logger('dev'));
+app.use(favicon(path.join(__dirname, 'public/images/favicon.ico')));
+app.use(logger('dev'));
 // use GZIP
-app.use(express.compress());
-app.use(express.urlencoded());
+app.use(compression());
 app.disable('x-powered-by');
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
-// app.set('env', 'development');
+app.set('env', 'development');
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+  app.use(errorHandler());
 }
 
 app.get('/', routes.index);
 app.get('/:number', routes.number);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+app.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+})
 
 // don't let Heroku put the app to sleep
 // http://quickleft.com/blog/6-easy-ways-to-prevent-your-heroku-node-app-from-sleeping
